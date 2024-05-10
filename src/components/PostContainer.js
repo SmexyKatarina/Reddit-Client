4bef01a0a82ef522d8bd542643646b383e11d742
Kat <mercier200012@hotmail.com>
2024-05-10 16:04:46 -0400

    Started the UseEffect for fetching posts on load

--- src/components/PostContainer.js
+++ src/components/PostContainer.js
@@ -1,10 +1,17 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+    fetchPosts
+} from '../store/redditSlice.js';
 
 export const PostContainer = () => {
     const reddit = useSelector((state) => state.reddit);
 
     const { isLoading, hasError } = reddit;
+
+    useEffect(() => {
+        useDispatch(fetchPosts());
+    });
 
     return (
         <>
